fa86069fd65e6abd676b679124e70d70087a6008
gregmelo <gregoryvericel6@gmail.com>
2025-06-06 11:03:39 +0200

    update oeuvre.php with connection function

--- oeuvre.php
+++ oeuvre.php
@@ -1,39 +1,34 @@
 <?php
-    require 'header.php';
-    require 'oeuvres.php';
+require 'header.php';
+// require 'oeuvres.php';
+require 'bdd.php';
 
-    // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
-    if(empty($_GET['id'])) {
-        header('Location: index.php');
-    }
+// Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
+if (empty($_GET['id'])) {
+    header('Location: index.php');
+}
+// Connexion à la base de données
+$bdd = connexion();
+$requete = $bdd->prepare('SELECT * FROM oeuvres WHERE id = ?');
+$requete->execute([$_GET['id']]);
+$oeuvre = $requete->fetch();
 
-    $oeuvre = null;
+// Si l'oeuvre n'existe pas, on redirige vers la page d'accueil
+if ($oeuvre === false) {
+    header('Location: index.php');
+    exit();
+}
 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
-
-    // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
-        header('Location: index.php');
-    }
 ?>
 
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?= htmlspecialchars($oeuvre['image']) ?>" alt="<?= htmlspecialchars($oeuvre['titre']) ?>">
     </div>
     <div id="contenu-oeuvre">
-        <h1><?= $oeuvre['titre'] ?></h1>
-        <p class="description"><?= $oeuvre['artiste'] ?></p>
-        <p class="description-complete">
-             <?= $oeuvre['description'] ?>
-        </p>
+        <h1><?= htmlspecialchars($oeuvre['titre']) ?></h1>
+        <p class="description"><?= htmlspecialchars($oeuvre['artiste']) ?></p>
+        <p class="description-complete"><?= htmlspecialchars($oeuvre['description']) ?></p>
     </div>
 </article>
 
